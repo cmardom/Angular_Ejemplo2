a5a88cb15a456d47e32ff0f53fd6feee4924b3ce
Carmen <cmardom599@g.educaand.es>
2024-01-19 14:33:42 +0100

    routing mal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './componentes/home/home.component';
+import { AboutComponent } from './componentes/about/about.component';
+import { LenguajesComponent } from './componentes/lenguajes/lenguajes.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"", component: HomeComponent
+    }, 
+    {
+        path:"about", component: AboutComponent
+    },
+    {
+        path: "**", pathMatch: "full", redirectTo: "home"
+    },
+    {
+        path: "lenguajes", component: LenguajesComponent
+    }
+];
